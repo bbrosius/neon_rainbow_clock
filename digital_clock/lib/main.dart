af5a649dd606d83e8353eeb3a433269d922870f1
Ben Brosius <benbrosius@gmail.com>
2020-01-17 11:52:19 -0500

    remote analog clock

--- digital_clock/lib/main.dart
+++ digital_clock/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'digital_clock.dart';
+import 'neon_digital_clock.dart';
 
 void main() {
   // A temporary measure until Platform supports web and TargetPlatform supports
@@ -29,6 +29,6 @@ void main() {
   // For more information, see the flutter_clock_helper package.
   //
   // Your job is to edit [DigitalClock], or replace it with your
-  // own clock widget. (Look in digital_clock.dart for more details!)
-  runApp(ClockCustomizer((ClockModel model) => DigitalClock(model)));
+  // own clock widget. (Look in neon_digital_clock.dart for more details!)
+  runApp(ClockCustomizer((ClockModel model) => NeonDigitalClock(model)));
 }
---
+++ digital_clock/lib/weather.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/model.dart';
+
+class Weather {
+  AssetImage getImageForWeatherCondition(
+      WeatherCondition condition, DateTime time) {
+    AssetImage weatherImage = AssetImage('icons/neon_sunny.png');
+
+    switch (condition) {
+      case WeatherCondition.cloudy:
+        if (time.hour > 19) {
+          weatherImage = AssetImage('icons/neon_night_cloudy.png');
+        } else {
+          weatherImage = AssetImage('icons/neon_cloudy.png');
+        }
+        break;
+      case WeatherCondition.foggy:
+        weatherImage = AssetImage('icons/neon_foggy.png');
+        break;
+      case WeatherCondition.rainy:
+        weatherImage = AssetImage('icons/neon_rainy.png');
+        break;
+      case WeatherCondition.snowy:
+        weatherImage = AssetImage('icons/neon_snow.png');
+        break;
+      case WeatherCondition.sunny:
+        if (time.hour > 19) {
+          weatherImage = AssetImage('icons/neon_clear_night.png');
+        } else {
+          weatherImage = AssetImage('icons/neon_sunny.png');
+        }
+        break;
+      case WeatherCondition.thunderstorm:
+        weatherImage = AssetImage('icons/neon_lightning.png');
+        break;
+      case WeatherCondition.windy:
+        weatherImage = AssetImage('icons/neon_windy.png');
+        break;
+    }
+
+    return weatherImage;
+  }
+
+  String getAccessibilityForTempUnit(TemperatureUnit unit) {
+    if (unit == TemperatureUnit.fahrenheit) {
+      return "degrees fahrenheit";
+    } else {
+      return "degrees celcius";
+    }
+  }
+}
